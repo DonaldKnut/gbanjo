b9926aaf0ac9509e6de4afbe9c14b04fa6ac894e
DonaldKnut <83928303+DonaldKnut@users.noreply.github.com>
2022-12-25 12:10:27 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from '../src/images/gbanjo-new.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">
+          <img src={logo} className="gbanjo-logo" alt="logo" />
+        </a>
+      </header>
+      <main>list style</main>
+    </div>
+  );
+}
+
+export default App;
